0b3dad41379afc05172de7ad8a79ccd2153ee17f
UnderDev <scottcoyne@hotmail.com>
2016-04-24 23:24:05 +0100

    Added barcode and Bike number entry

--- AdminPages/Administration.aspx
+++ AdminPages/Administration.aspx
@@ -2,27 +2,37 @@
     CodeFile="Administration.aspx.cs" Inherits="AdminPages_Administration" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+    <style type="text/css">
+        .style1
+        {
+            width: 400px;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
-    <div>
+    
+    <div class="AdminToolsBg">
         <br />
-        Enter The Bike Number or Barcode of the bike you want to Edit
-        <table align="center" width="400" >
+        <h3>Update The "Bike in Use" Field by the Bike Number or Barcode</h3>
+        <table align="center" class="style1" >
             <tr>
                 <td style="width: 100px">
-                    <asp:TextBox ID="txtBoxBarcode" runat="server"></asp:TextBox>
+                    <asp:TextBox ID="txtBoxBarcode" runat="server" placeholder="E.g. 23"></asp:TextBox>
                 </td>
-                <td style="width: 50px">
-                    <asp:DropDownList ID="DropDownList1" runat="server">
+                <td style="width: 100px">
+                    <asp:DropDownList ID="ddlBikeInUse" runat="server">
                         <asp:ListItem Value="Y">Yes</asp:ListItem>
                         <asp:ListItem Value="N">No</asp:ListItem>
                     </asp:DropDownList>
                 </td>
-                <td style="width: 50px">
-                    <asp:Button ID="Button1" runat="server" Text="Button" onclick="Button1_Click" />
+                <td style="width: 100px">
+                    <asp:Button ID="btnUpdate" runat="server" Text="Update" 
+                        onclick="btnUpdate_Click" Width="100px" />
                 </td>
             </tr>
         </table>
+        <br />
+        <asp:Label ID="lblErrorMessage" runat="server" ></asp:Label>
         <br />
     </div>
 </asp:Content>
--- AdminPages/Administration.aspx.cs
+++ AdminPages/Administration.aspx.cs
@@ -16,29 +16,36 @@ public partial class AdminPages_Administration : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+        txtBoxBarcode.Focus();
     }
 
     //ADD LABLES FOR ERROR HANDLING AND MAKE FANCY ///// ALSO CHANGE TO ALLOW THE USER TO ENTER A BIKE NUMBER OR BARCODE
-    protected void Button1_Click(object sender, EventArgs e)
+    protected void btnUpdate_Click(object sender, EventArgs e)
     {
-        String barcode = txtBoxBarcode.Text;
-
         _conn.Close();
         _conn.Open();
 
-        try
-        {
-            String curDate = "UPDATE BikeRentalTbl SET BcInUse= '" + DropDownList1.SelectedValue + "' where BarCode ='" + barcode + "'";
+        string barcode = txtBoxBarcode.Text;
 
-            _com = new SqlCommand(curDate, _conn);
-            _com.ExecuteScalar();
-        }
-        catch (Exception)
-        {
-            //lblUserMessage.Text = "The Barcode You Entered Could Not Be Found!";
-        }
-        _conn.Close();
+        //Update the BcInUse column with the selected value in the dropdownlist based on either the bike number entered or the full barcode
+        String sqlUpdate = "UPDATE BikeRentalTbl SET BcInUse = '" + ddlBikeInUse.SelectedValue + "' WHERE (BarCode = '" + barcode + "') OR (SUBSTRING(BarCode, 2, 2) = '" + txtBoxBarcode.Text + "')";
+        _com = new SqlCommand(sqlUpdate, _conn);
+        int checkUpdated = _com.ExecuteNonQuery();
 
+        //Check that the database was updated, if the command returns 1, a row has been updated, otherwise the string entered was not found in the database 
+        if (checkUpdated == 1){
+            lblErrorMessage.Text = "Bike " + barcode + " Found! <br/> Successfully Updated the Bike In Use value to " + ddlBikeInUse.Text;
+        }
+        else{
+            lblErrorMessage.Text = "Bike Not Found, Try Again";
+        }
+        //reset the textbox
+        txtBoxBarcode.Text = "";
+
+       _conn.Close();
+    }
+    protected void txtBoxBarcode_TextChanged(object sender, EventArgs e)
+    {
+        txtBoxBarcode.Text = "";
     }
 }
